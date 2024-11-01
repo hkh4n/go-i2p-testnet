8051c22e4e8d48f03cef99becdcf926a740b3155
Haris Khan <hk@hariskhan.dev>
2024-10-31 20:04:38 -0400

    added logging to lib/docker_control/volume.go

--- lib/docker_control/volume.go
+++ lib/docker_control/volume.go
@@ -9,12 +9,26 @@ import (
 
 func CreateSharedVolume(cli *client.Client, ctx context.Context) (string, error) {
 	volumeName := "go-i2p-shared"
+
+	log.WithField("volumeName", volumeName).Debug("Starting Docker volume creation")
+
 	createOptions := volume.CreateOptions{
 		Name: volumeName,
 	}
+
+	log.WithFields(map[string]interface{}{
+		"volumeName": volumeName,
+		"options":    createOptions,
+	}).Debug("Creating Docker volume")
+
 	_, err := cli.VolumeCreate(ctx, createOptions)
 	if err != nil {
+		log.WithFields(map[string]interface{}{
+			"volumeName": volumeName,
+			"error":      err,
+		}).Error("Failed to create Docker volume")
 		return "", fmt.Errorf("error creating shared volume: %v", err)
 	}
+	log.WithField("volumeName", volumeName).Debug("Successfully created Docker volume")
 	return volumeName, nil
 }
